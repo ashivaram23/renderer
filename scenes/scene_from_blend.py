65bfb8560dadc896be779187266f553460ff6553
Amogh Shivaram <ashivaram@wisc.edu>
2024-01-07 02:41:11 -0600

    write script to convert blend to scene file

---
+++ scenes/scene_from_blend.py
@@ -0,0 +1,114 @@
+"""
+Creates a scene file for the renderer from a Blender project.
+
+To use this script, run Blender in the command line from the folder where you
+want to put the scene JSON and OBJ files. Command line launching is described at
+docs.blender.org/manual/en/latest/advanced/command_line/launch/index.html.
+
+Use the following arguments:
+blender yourfile.blend --python scene_from_blend.py
+
+You can also open a .blend file manually, navigate to the Scripting tab, and
+load and run this script.
+"""
+
+
+import bpy
+import math
+import json
+import os
+
+
+def xzy_to_xyz(values):
+    return [values[0], values[2], values[1]]
+
+
+def add_vec(a, b):
+    return [x + y for (x, y) in zip(a, b)]
+
+
+# TODO fix this, currently doesn't work right
+def multiply_quats(a, b):
+    w = a[0] * b[0] - a[1] * b[1] - a[2] * b[2] - a[3] * b[3]
+    x = a[0] * b[1] + a[1] * b[0] - a[2] * b[3] + a[3] * b[2]
+    y = a[0] * b[2] + a[1] * b[3] + a[2] * b[0] - a[3] * b[1]
+    z = a[0] * b[3] - a[1] * b[2] + a[2] * b[1] + a[3] * b[0]
+    return [w, x, y, z]
+
+
+def rotate_quat(vec, quat):
+    inv_quat = [quat[0]] + [-x for x in quat[1:4]]
+    rotated = multiply_quats(quat, multiply_quats([0] + vec, inv_quat))
+    return rotated[-3:]
+
+
+scene_dict = {"camera": {}, "settings": {}, "objects": {}}
+scene = bpy.context.scene
+
+origin = xzy_to_xyz(scene.camera.location[:3])
+camera_quat = list(scene.camera.rotation_quaternion[:4])
+look_at = add_vec(origin, xzy_to_xyz(rotate_quat([0, 0, -1], camera_quat)))
+up = add_vec(origin, xzy_to_xyz(rotate_quat([0, 1, 0], camera_quat)))
+
+scene_dict["camera"] = {
+    "film_dimensions": [scene.render.resolution_x, scene.render.resolution_y], 
+    "origin": origin,
+    "look_at": look_at,
+    "up": up,
+    "field_of_view": math.degrees(scene.camera.data.angle)
+    }
+
+environment = [0.8] * 3
+if "Background" in scene.world.node_tree.nodes:
+    background_node = scene.world.node_tree.nodes["Background"]
+    environment = list(background_node.inputs[0].default_value[:3])
+
+scene_dict["settings"] = {
+    "environment": environment,
+    "samples_per_pixel": scene.cycles.samples,
+    "max_ray_depth": scene.cycles.max_bounces
+    }
+
+bpy.ops.object.select_all(action="DESELECT")
+export_args = {
+    "forward_axis": "Z",
+    "up_axis": "Y",
+    "export_selected_objects": True,
+    "export_triangulated_mesh": True, 
+    "export_uv": False, 
+    "export_normals": False, 
+    "export_materials": False
+    }
+
+os.makedirs("objects", exist_ok=True)
+meshes = [obj for obj in bpy.data.objects if obj.type == "MESH"]
+for mesh in meshes:
+    mesh.select_set(True)
+    bpy.ops.wm.obj_export(filepath=f"objects/{mesh.name}.obj", **export_args)
+    mesh.select_set(False)
+
+    reflectance = [0.5] * 3
+    materials = mesh.data.materials.values()
+    if len(materials) > 0 and "Diffuse BSDF" in materials[0].node_tree.nodes:
+        diffuse_node = materials[0].node_tree.nodes["Diffuse BSDF"]
+        reflectance = list(diffuse_node.inputs[0].default_value[:3])
+
+    scene_dict["objects"][mesh.name] = {
+        "type": "mesh",
+        "file": f"{os.getcwd()}/objects/{mesh.name}.obj",
+        "material": {"type": "diffuse", "reflectance": reflectance}
+        }
+
+blend_name = bpy.path.basename(bpy.data.filepath)
+output_name = blend_name[:-6] if blend_name.endswith(".blend") else "untitled"
+output_filename = f"{output_name}.json"
+for i in range(1, 100):
+    if os.path.exists(output_filename):
+        output_filename = f"{output_name}_{i}.json"
+    else:
+        break
+
+with open(output_filename, "w") as file:
+    json.dump(scene_dict, file)
+
+print(f"Created scene file at {output_filename}")
